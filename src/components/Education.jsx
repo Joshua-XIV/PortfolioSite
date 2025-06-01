89877fb02e8eb2c55dddcd161f0f6707ec731588
Joshua <josh.hdz956@yahoo.com>
2025-06-01 09:47:46 -0500

    added uni

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,12 +1,13 @@
-const Education = ({name, degreeType, degreeMajor, startingDate, endingDate = "Present", imagePath}) => {
+const Education = ({name, degreeType, degreeMajor, startingDate, endingDate = "Present", imagePath, scale = 100}) => {
   if (endingDate == "")
     endingDate = "Present"
+
+  const size = `${scale}%`
 
   return (
     <section className='px-4 py-2'>
       <div className='text-white flex space-x-4 items-center'>
-        <div className="w-15 h-15 rounded-full border-green-500 border-2 overflow-hidden">
-          <img src={imagePath} alt={name} className='scale-150 bg-white'></img>
+        <div className="w-15 h-15 rounded-full bg-white bg-center bg-no-repeat bg-cover border-green-500 border-2 overflow-hidden" style={{ backgroundImage: `url(${imagePath})`, backgroundSize: size }}>
         </div>
         <div className="flex flex-col">
           <text className='text-indigo-200 text-2xl sm:text-2xl md:text-[14px]'>{startingDate} - {endingDate}</text>
