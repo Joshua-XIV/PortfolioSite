380f41be89a37a190fea3a6151a782a101aa3d84
Joshua <josh.hdz956@yahoo.com>
2026-07-17 21:03:05 -0700

    why not

--- src/components/Educations.jsx
+++ src/components/Educations.jsx
@@ -1,42 +1,21 @@
 import Education from "./Education"
-import OregonLogo from "../assets/oregonstate.svg"
-import SNHULogo from "../assets/SNHU.png"
+import schools from "../data/schools"
 import useTypeOnVisible from "../hooks/useTypeOnVisible"
 
-const schools = [
-  {
-    name: "Southern New Hampshire University",
-    degreeType: "B.S", 
-    degreeMajor: "Computer Science", 
-    startingDate: "January", 
-    endingDate: "December 2026", 
-    imagePath: SNHULogo, 
-    scale: 120
-  },
-  {
-    name: "Oregon State University", 
-    degreeType: "B.S", 
-    degreeMajor: "Computer Science", 
-    startingDate: "September 2023", 
-    endingDate: "December 2025", 
-    imagePath: OregonLogo, 
-    scale: 75
-  },
-]
+const Educations = ({ gradient }) => {
+  const { typedText, ref, isDone } = useTypeOnVisible("Education", 100)
 
-
-const Educations = ({gradient}) => {
-  const {typedText, ref, isDone} = useTypeOnVisible("Education", 100);
   return (
     <section className="px-2">
-      <div ref={ref} className={`mt-20 mb-10 text-5xl text-center ${gradient}`}>{typedText}
+      <div ref={ref} className={`mt-20 mb-10 text-5xl text-center ${gradient}`}>
+        {typedText}
         {!isDone && <span className={`text-5xl ${gradient} cursor`}>|</span>}
       </div>
 
       <div className="px-2 relative">
         <div className="absolute left-16 top-0 bottom-0 w-0.5 bg-gradient-to-br from-blue-500 to-green-500 -z-1"></div>
         <div className="gradient-border border-2 rounded-xl p-2 space-y-10">
-          {schools.map(({name, degreeType, degreeMajor, startingDate, endingDate, imagePath, scale}, idx) => (
+          {schools.map(({ name, degreeType, degreeMajor, startingDate, endingDate, imagePath, scale }, idx) => (
             <Education
               key={idx}
               name={name}
---
+++ src/data/schools.js
@@ -0,0 +1,25 @@
+import OregonLogo from "../assets/oregonstate.svg"
+import SNHULogo from "../assets/SNHU.png"
+
+const schools = [
+  {
+    name: "Southern New Hampshire University",
+    degreeType: "B.S",
+    degreeMajor: "Computer Science",
+    startingDate: "January 2026",
+    endingDate: "December 2026",
+    imagePath: SNHULogo,
+    scale: 120
+  },
+  {
+    name: "Oregon State University",
+    degreeType: "B.S",
+    degreeMajor: "Computer Science",
+    startingDate: "September 2023",
+    endingDate: "December 2025",
+    imagePath: OregonLogo,
+    scale: 75
+  },
+]
+
+export default schools
