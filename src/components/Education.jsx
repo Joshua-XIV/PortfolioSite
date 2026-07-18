9c8df72b99cbeb4a316fad0c5d4e65f82d8fe46a
Joshua <josh.hdz956@yahoo.com>
2026-07-17 18:11:31 -0700

    fixed education image bug

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,7 +8,7 @@ const Education = ({name, degreeType, degreeMajor, startingDate, endingDate = "P
     <section className='px-4 py-2'>
       <div className='text-white flex space-x-4 items-center'>
         <div 
-          className="w-15 h-15 rounded-full bg-white bg-center bg-no-repeat bg-cover border-green-500 border-2 overflow-hidden"
+          className="w-16 h-16 shrink-0 rounded-full bg-white bg-center bg-no-repeat bg-cover border-green-500 border-2 overflow-hidden"
           style={{ backgroundImage: `url(${imagePath})`, backgroundSize: size }}>
         </div>
         <div className="flex flex-col">
